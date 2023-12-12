ad4223fe34d672066fffac8c0197ab57dc77e010
Василий Кузнецов <vasekku25@gmail.com>
2023-12-12 18:30:10 +0700

    задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿void Main(string[] args)
+{
+string input = "шалаш";
+Console.WriteLine(isPalindrome ? "Да" : "Нет");
+}
+bool IsPalindrome(string str)
+{
+string normalized = new
+string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+return normalized.SequenceEqual(normalized.Reverse());
+}

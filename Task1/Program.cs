7ad832f13932da2d06f285c215bd250eb597f984
Василий Кузнецов <vasekku25@gmail.com>
2023-12-12 17:36:54 +0700

    1 задача сделана

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,18 @@
 ﻿
+        char[,] charArray = {
+            { 'П', 'Р', 'И', 'В', 'Е', 'Т' },
+            { 'М', 'Е', 'Д', 'В', 'Е', 'Д' }
+        };
+
+        string result = "";
+
+        for (int i = 0; i < charArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < charArray.GetLength(1); j++)
+            {
+                result += charArray[i, j];
+            }
+        }
+
+        Console.WriteLine("Строка из символов массива: " + result);
+    
